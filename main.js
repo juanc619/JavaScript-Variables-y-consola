07b91697e983e555880bd968595a14e551210b27
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-03-30 18:23:28 -0500

    js var-consol

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-let 
+let nombre = "Juan carlos";
+let apellido = "Velasquez";
+let nombreApeliido = nombre + " " + apellido;
+
+nombre = "juanca";
+
+let anioActual = 2022;
+let anioNacimiento = 1986;
+let edad = anioActual - anioNacimiento;
+
+const IVA = 21;
+
+console.log(edad);
+
+let nombreUsuario = prompt("ingrese su nombre");
+console.log (nombreUsuario);
+
+alert("Hola " + nombreUsuario + ".");
